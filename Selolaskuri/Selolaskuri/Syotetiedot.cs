6290eae5648c6a09b008e65259f443a2079cc32b
isuihko <34057045+isuihko@users.noreply.github.com>
2018-06-19 09:40:18 +0300

    Modified constructor
    Use constructor chaining to avoid repetition.

--- Selolaskuri/Selolaskuri/Syotetiedot.cs
+++ Selolaskuri/Selolaskuri/Syotetiedot.cs
@@ -2,13 +2,14 @@
 // Luokka syötetietoja varten
 //
 // Public:
-//      Syotetiedot() ilman parametreja ja parametrein
+//      Syotetiedot()           konstruktori ilman parametreja ja parametrein
 //      UudenPelaajanLaskenta
 //
 // Luotu 5.4.2018 Ismo Suihko
 //
 // Muutokset:
 // 16.-17.6.2018  pientä järjestelyä, lisätty tarkistus UudenPelaajanLaskenta()
+// 19.6.2018      constructor: poistettu toistoa käyttämällä konstruktorien ketjutusta (constructor chaining)
 
 namespace Selolaskuri
 {
@@ -34,20 +35,18 @@ namespace Selolaskuri
 
         public Ottelulista ottelut;   // sis. vastustajien selot ja ottelutulokset
 
-        public Syotetiedot()
-        {
-            miettimisaika              = Vakiot.Miettimisaika_enum.MIETTIMISAIKA_VAH_90MIN;  // =oletus
-            alkuperainenSelo           = 0;
-            alkuperainenPelimaara      = 0;
-            vastustajanSeloYksittainen = 0;
-            vastustajienSelot_str      = null;
-            ottelunTulos               = Vakiot.OttelunTulos_enum.TULOS_MAARITTELEMATON;
-            ottelut                    = new Ottelulista();
+
+        // Oikeastaan kaikkea ei tarvitsisi alustaa, koska tiedot täytetään lomakkeelta
+        // ks. SelolaskuriForm.cs/HaeSyotteetLomakkeelta()
+        //
+        // Ottelulista pitää kuitenkin luoda ja tehdään nyt kaikki muukin alustus
+        public Syotetiedot() : this(Vakiot.Miettimisaika_enum.MIETTIMISAIKA_VAH_90MIN, null, null, null, Vakiot.OttelunTulos_enum.TULOS_MAARITTELEMATON)
+        {          
         }
 
         // KÄYTETÄÄN TESTATTAESSA (UnitTest)
-        // Esim. Syotetiedot ottelu =
-        //    new Syotetiedot(Vakiot.Miettimisaika_enum.MIETTIMISAIKA_VAH_90MIN, "1525", "0", "1525", Vakiot.OttelunTulos_enum.TULOS_VOITTO);
+        // esim. Syotetiedot ottelu =
+        //   new Syotetiedot(Vakiot.Miettimisaika_enum.MIETTIMISAIKA_VAH_90MIN, "1725", "1", "1441", Vakiot.OttelunTulos_enum.TULOS_VOITTO);
         public Syotetiedot(Vakiot.Miettimisaika_enum aika, string selo, string pelimaara, string vastustajat, Vakiot.OttelunTulos_enum tulos)
         {
             miettimisaika              = aika;
@@ -55,11 +54,20 @@ namespace Selolaskuri
             alkuperainenPelimaara_str  = pelimaara;
             vastustajienSelot_str      = vastustajat;
             ottelunTulos               = tulos;
-            ottelut                    = new Ottelulista();
+
+            // Clear these too although not actually needed
+            alkuperainenSelo            = 0;
+            alkuperainenPelimaara       = 0;
+            vastustajanSeloYksittainen  = 0;
+            
+            // Create en empty list for matches (opponent's selo, match result)
+            ottelut = new Ottelulista();
         }
 
         // Uuden pelaajan laskennassa ja tulostuksissa joitain erikoistapauksia
-        // Tarkistetaan alkuperäisestä pelimäärästä
+        //
+        // Tarkistetaan alkuperäisestä pelimäärästä, koska turnauksen laskenta tehdään
+        // uuden pelaajan kaavalla vaikka pelimäärä laskennan aikana ylittäisikin rajan
         public bool UudenPelaajanLaskenta()
         {
             return (alkuperainenPelimaara >= Vakiot.MIN_PELIMAARA &&

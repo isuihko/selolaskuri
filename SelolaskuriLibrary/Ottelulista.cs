ae4a5a3a2a03293f7ed739e76d040fcc2511e1da
isuihko <34057045+isuihko@users.noreply.github.com>
2018-08-26 10:00:52 +0300

    SelolaskuriLibrary for checking and calculation

---
+++ SelolaskuriLibrary/Ottelulista.cs
@@ -0,0 +1,109 @@
+﻿//
+// Store the played matches (one or many) and the needed routines
+//
+// Public:
+//      LisaaOttelunTulos  - adds a new match with opponent selo and the result (lost/draw/won)
+//      Lukumaara          - returns number of stored matches
+//      Keskivahvuus       - returns average opponent strength / selo strength
+//      HaeEnsimmainen     - returns the first match
+//      HaeSeuraava        - returns the next match
+//
+// 2.4.2018 Ismo Suihko
+//
+// Created in Syotetiedot. Ottelut = new Ottelulista();
+// Data is stored in SelolaskuriOperations
+// Data is fetched for calculations in Selolaskuri PelaaKaikkiOttelut().
+// 
+// Public:
+//  LisaaOttelunTulos() - store a new match
+//  HaeEnsimmainen()    - return the first match
+//  HaeSeuraava()       - return the next match
+//  Lukumaara           - number of matches
+//  Keskivahvuus        - average strength of the opponents
+// 
+// Modifications:
+//   19.7.2018  Data is hidden, added routines Lukumaara, Keskivahvuus, HaeEnsimmainen ja HaeSeuraava
+//
+
+using System;  // needed for Tuple
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelolaskuriLibrary {
+    //
+    // Ottelujen tietojen tallennus listaan, vastustajan vahvuusluku ja ottelun tulos
+    //
+    public class Ottelulista : IEnumerable
+    {
+        private struct Ottelu
+        {
+            public int VastustajanSelo { get; }
+            public Vakiot.OttelunTulos_enum OttelunTulos { get; }
+
+            public Ottelu(int selo, Vakiot.OttelunTulos_enum tulos)  // tallentaa
+            {
+                VastustajanSelo = selo;
+                OttelunTulos = tulos;
+            }
+        }
+
+        // Lista ottelutuloksille, jossa vastustajan selo sekä ottelun tulos
+        // Pitää voida lisätä alkioita (Add), käydä ne läpi alkuperäisessä järjestyksessä,
+        // hakea lukumäärä (Count) sekä laskea alkioista keskiarvo (Average(x => x.vastustajanSelo))
+        private IList<Ottelu> tallennetutOttelut = new List<Ottelu>();
+
+        // Ottelutuloksen (vastustaja ja tulos) lisääminen listaan
+        public void LisaaOttelunTulos(int vastustajanSelo, Vakiot.OttelunTulos_enum ottelunTulos)
+        {
+            Ottelu ottelu = new Ottelu(vastustajanSelo, ottelunTulos);
+            tallennetutOttelut.Add(ottelu);
+        }
+
+        private int _index;
+
+        private Tuple<int, Vakiot.OttelunTulos_enum> HaeOttelu(int index)
+        {
+            if (index < Lukumaara)
+                return Tuple.Create(tallennetutOttelut[index].VastustajanSelo, tallennetutOttelut[index].OttelunTulos);
+            else
+                return Tuple.Create(0, Vakiot.OttelunTulos_enum.TULOS_MAARITTELEMATON);
+        }
+
+        // Returns the first match  HaeEnsimmainen()
+        // Returns the next match   HaeSeuraava()
+        // If no more matchess left, returns TULOS_MAARITTELEMATON
+        public Tuple<int, Vakiot.OttelunTulos_enum> HaeEnsimmainen()
+        {
+            _index = 0;
+            return HaeOttelu(_index);
+        }
+
+        public Tuple<int, Vakiot.OttelunTulos_enum> HaeSeuraava()
+        {
+            _index++;
+            return HaeOttelu(_index);
+        }
+
+        public int Lukumaara {
+            get {
+                return tallennetutOttelut.Count;
+            }
+        }
+
+        public int Keskivahvuus {
+            get {
+                // Found out that Math.Round(1568.5) was 1568 but should had been 1569 like in Java.
+                // ...so adding 0.01 to get correct result...
+                // https://stackoverflow.com/questions/977796/why-does-math-round2-5-return-2-instead-of-3
+                // XXX: Need to check also other Math.Round() usages?
+                return (int)Math.Round(tallennetutOttelut.Average(x => x.VastustajanSelo) + 0.01); // Linq
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)tallennetutOttelut).GetEnumerator();
+        }
+    }
+}
---
+++ SelolaskuriLibrary/Vakiot.cs
@@ -0,0 +1,87 @@
+﻿//
+// All the constants
+//
+// 7.1.2018 Ismo Suihko
+//
+// Modifications:
+//   17.6.2018      Added UUDEN_PELAAJAN_ALKUSELO
+//   4.8.2018       Added SYOTE_VIRHE_CSV_FORMAT
+//                  defined initial values for MIETTIMISAIKA constants according to minutes they represent
+//   12.8.2018      Added clipboard handling related constants LEIKEKIRJA_*
+//   15.8.2018      Added TULOS_EI_ANNETTU and TURNAUKSEN_TULOS_ANTAMATTA
+//    
+
+namespace SelolaskuriLibrary {
+
+    public class Vakiot {
+        // YLEISET VAKIOT, joilla määrätään syötteen rajat
+        public const int MIN_SELO       = 1000;
+        public const int MAX_SELO       = 2999;
+        public const int MIN_PELIMAARA  = 0;
+        public const int MAX_PELIMAARA  = 9999;
+
+        // Vahvuuslukuarvon maksimipituus
+        // Pituus 5  Sisältää ottelun tuloksen kertovan +, - tai =
+        public const int MAX_PITUUS  = 5; 
+        public const int SELO_PITUUS = 4;
+
+        // Kun ottelumäärä on 0-10, käytetään uuden pelaajan laskentakaavaa
+        public const int MAX_PELIMAARA_UUSI_PELAAJA = 10;
+        public const int UUDEN_PELAAJAN_ALKUSELO    = 1525;
+
+        // syötteen tarkastuksessa käytetyt virhestatukset eivät ole syötteiden arvoarvoalueella
+        public const int SYOTE_STATUS_OK                = 0;
+        public const int SYOTE_VIRHE_MIETTIMISAIKA      = -1; // Tälle ei virheilmoitusta, koska toistaiseksi ei mahdollinen
+        public const int SYOTE_VIRHE_OMA_SELO           = -2;
+        public const int SYOTE_VIRHE_VASTUSTAJAN_SELO   = -3;
+        public const int SYOTE_VIRHE_PELIMAARA          = -4;
+        public const int SYOTE_VIRHE_BUTTON_TULOS       = -5;
+        public const int SYOTE_VIRHE_YKSITTAINEN_TULOS  = -6;
+        public const int SYOTE_VIRHE_TURNAUKSEN_TULOS   = -7;
+        public const int SYOTE_VIRHE_CSV_FORMAT         = -8;
+
+        public const int PELIMAARA_TYHJA = -1; // OK, muilla kuin uusilla pelaajilla voi olla tyhjä
+
+        public const int LEIKEKIRJA_MAX_RIVINPITUUS     = 1000;
+        public const int LEIKEKIRJA_MAX_RIVIMAARA       = 100;
+
+        public const int TURNAUKSEN_TULOS_ANTAMATTA     = -1;
+        // käytännössä maksimi on pelaajien lkm, koska ei voida saada enempää pisteitä kuin on otteluita
+        // Jos on pelattu kahdesti samaa pelaajaa vastaan, niin ne annetaan erillisinä otteluina
+        public const float TURNAUKSEN_TULOS_MAX         = 199.5F;  
+
+
+        // Tallenna kunkin ottelun tulos kokonaislukuna kahdella kerrottuna lukujen 0, 1/2 ja 1 sijaan.
+        //
+        // TULOS_EI_ANNETTU: kun turnauksen tuloksissa ei ole vahvuusluvun kanssa tulosta +-=
+        //                   Esim. "+1624 -1700 1685 +1400" tai "1.5 1525 1441 1973 1718 1784 1660 1966"
+        //
+        // TULOS_MAARITTELEMATON: Kun tulosta ei ole vielä saatu tietoon. Myös lopetusehto ottelulistaa tutkittaessa.
+        //
+        public enum OttelunTulos_enum
+        {
+            TULOS_EI_ANNETTU = -2,          
+            TULOS_MAARITTELEMATON = -1,
+            TULOS_TAPPIO = 0,
+            TULOS_TASAPELI = 1,
+            TULOS_VOITTO = 2
+        };
+
+        // Miettimisajat (miettimisajan pituuden mukaan nousevassa järjestyksessä)
+        public enum Miettimisaika_enum {
+            MIETTIMISAIKA_MAARITTELEMATON = -1,
+            MIETTIMISAIKA_ENINT_10MIN = 10,
+            MIETTIMISAIKA_11_59MIN = 59,
+            MIETTIMISAIKA_60_89MIN = 89,
+            MIETTIMISAIKA_VAH_90MIN = 90
+        };
+
+        // Miettimisaikojen vaihdon takia vaihdetaan kenttien tekstejä. SELO on pitkä peli ja PELO on pikashakki.
+        // Esim. "Oma SELO" ja "Oma PELO".
+        public enum VaihdaMiettimisaika_enum { 
+            VAIHDA_SELOKSI,
+            VAIHDA_PELOKSI
+        };
+    }
+}
+
